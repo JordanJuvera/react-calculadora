65b3b4b50bf2091efe9e2d4448ada0c60fac478f
JordanJuvera <juvera.jordan@gmail.com>
2022-04-10 14:45:05 -0500

    Proyecto Calculadora Final ver_1.0

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import '../hojas-de-estilo/Boton.css'
 
 
 function Boton(props){
 
-    const esOperador = valor =>{ return isNaN(valor) && (valor != '.') && (valor != '=')}
+    const esOperador = valor =>{ return isNaN(valor) && (valor.trim() !== '.') && (valor.trim() !== '=')}
 
    return (
-       <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' } `.trimEnd()}>
+       <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' } `.trimEnd()} 
+        onClick={() => props.manejarClic(props.children)}
+       > 
             {props.children}           
        </div>
    )
